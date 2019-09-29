05ede40557a6a356715796182c233876bbb96f36
Yunlin <mao.looper@qq.com>
2019-09-29 13:48:04 +0800

    0927-myl-update gui

--- src/main/java/FrontEnd/parts/MPopMenu.java
+++ src/main/java/FrontEnd/parts/MPopMenu.java
@@ -9,37 +9,50 @@ import java.awt.event.KeyEvent;
 import FrontEnd.MainWindow;
 
 
-public class MPopMenu extends JPopupMenu implements ActionListener {
+public class MPopMenu extends JPanel implements ActionListener {
 
     private MainWindow mainWindow;
     private JPopupMenu editMenu;
     private JPopupMenu fileMenu;
     private FileOperation fileOperation;
 
-    public MPopMenu(MainWindow mainWindow){
+    private JMenuItem cut;
+    private JMenuItem copy;
+    private JMenuItem paste;
+    private JMenuItem open;
+    private JMenuItem newFile;
+    private JMenuItem saveFile;
+
+    public MPopMenu(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
     }
 
-    public void init(){
+    public void init() {
         this.editMenu = new JPopupMenu();//ÄãµÄµ¯³ö²Ëµ¥
-        JMenuItem cut=new JMenuItem("Cut");
+
+        cut = new JMenuItem("Cut");
         setShortcut(cut, KeyEvent.VK_X);
-        JMenuItem copy=new JMenuItem("Copy");
+
+        copy = new JMenuItem("Copy");
         setShortcut(copy, KeyEvent.VK_C);
-        JMenuItem paste=new JMenuItem("Paste");
+
+        paste = new JMenuItem("Paste");
         setShortcut(paste, KeyEvent.VK_V);
         editMenu.add(cut);
         editMenu.add(copy);
         editMenu.add(paste);
 
         this.fileMenu = new JPopupMenu();
-        JMenuItem open=new JMenuItem("Open");
+
+        open = new JMenuItem("Open");
         setShortcut(open, KeyEvent.VK_O);
         open.addActionListener(this);
-        JMenuItem newFile=new JMenuItem("New");
+
+        newFile = new JMenuItem("New");
         setShortcut(newFile, KeyEvent.VK_N);
         newFile.addActionListener(this);
-        JMenuItem saveFile=new JMenuItem("Save");
+
+        saveFile = new JMenuItem("Save");
         setShortcut(saveFile, KeyEvent.VK_S);
         saveFile.addActionListener(this);
         fileMenu.add(open);
@@ -49,34 +62,29 @@ public class MPopMenu extends JPopupMenu implements ActionListener {
         fileOperation = new FileOperation(mainWindow);
     }
 
-    public JPopupMenu getEditMenu(){
+    public JPopupMenu getEditMenu() {
         return editMenu;
     }
 
-    public JPopupMenu getFileMenu(){
+    public JPopupMenu getFileMenu() {
         return fileMenu;
     }
 
 
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("Save"))
-        {
+        if (e.getActionCommand().equals("Save")) {
             fileOperation.save();
         }
-        if(e.getActionCommand().equals("New"))
-        {
+        if (e.getActionCommand().equals("New")) {
             fileOperation.creat();
         }
-        if(e.getActionCommand().equals("Open"))
-        {
+        if (e.getActionCommand().equals("Open")) {
             fileOperation.open();
         }
     }
 
-    private void setShortcut(JMenuItem jmi, int key)
-    {
+    private void setShortcut(JMenuItem jmi, int key) {
         KeyStroke keyStroke = KeyStroke.getKeyStroke(key, InputEvent.CTRL_MASK);
         jmi.setMnemonic(key);
         jmi.setAccelerator(keyStroke);
