b0a98e38c1acabe628f0400f86b9cd78d3d9025c
zjj <632291793@qq.com>
2019-10-18 10:58:22 +0800

    back end: modify grammar file

--- src/main/java/interpreter/lexer/Lexer.java
+++ src/main/java/interpreter/lexer/Lexer.java
@@ -139,6 +139,8 @@ public class Lexer {
                         }
                         if (peek == EOF) {
                             return null;
+                        } else {
+                            ++curLine;
                         }
                         getNextChar();
                         goon = true;
