7d989d64ce02d6581a479c8c4ff4d260b1c24ed6
WHU-Yh-jx <1092949763@qq.com>
2019-10-31 01:14:33 +0800

    1031-hjx-Write While

--- src/main/java/interpreter/executor/subExecutor/WhileStmt.java
+++ src/main/java/interpreter/executor/subExecutor/WhileStmt.java
@@ -1,4 +1,36 @@
 package interpreter.executor.subExecutor;
 
-public class WhileStmt {
+import interpreter.env.Env;
+import interpreter.executor.BaseExecutor;
+import interpreter.executor.Executor;
+import interpreter.executor.ExecutorFactory;
+import interpreter.grammar.lalr.LALRNonterminalSymbol;
+import interpreter.intermediate.node.INode;
+
+public class WhileStmt extends BaseExecutor {
+    public WhileStmt(Env env) {
+        super(env);
+    }
+
+    @Override
+    public Object Execute(INode root) throws Exception {
+        if (!root.getSymbol().equals(LALRNonterminalSymbol.WHILE_STMT)) {
+            System.out.println("parse error in while stmt");
+
+        }
+        //while-stmt -> while(expr) compound-stmt
+        else {
+            ExecutorFactory factory = ExecutorFactory.getExecutorFactory();
+            Executor exeExpr = factory.getExecutor(root.getChild(2), env);
+            Executor exeCompound = factory.getExecutor(root.getChild(4), env);
+            Object[] exeValue1 = (Object[]) exeExpr.Execute(root.getChild(2));
+            while ((boolean)exeValue1[1])
+            {
+                Object[] exeValue2 = (Object[]) exeCompound.Execute(root.getChild(4));
+                exeValue1 = (Object[]) exeExpr.Execute(root.getChild(2));
+            }
+
+        }
+        return null;
+    }
 }
--- src/main/java/interpreter/executor/subExecutor/WriteStmt.java
+++ src/main/java/interpreter/executor/subExecutor/WriteStmt.java
@@ -1,4 +1,32 @@
 package interpreter.executor.subExecutor;
 
-public class WriteStmt {
+import interpreter.env.Env;
+import interpreter.executor.BaseExecutor;
+import interpreter.executor.Executor;
+import interpreter.executor.ExecutorFactory;
+import interpreter.grammar.lalr.LALRNonterminalSymbol;
+import interpreter.intermediate.node.INode;
+
+public class WriteStmt extends BaseExecutor {
+    public WriteStmt(Env env) {
+        super(env);
+    }
+
+    @Override
+    public Object Execute(INode root) throws Exception {
+        if (!root.getSymbol().equals(LALRNonterminalSymbol.WRITE_STMT)) {
+            System.out.println("parse error in write stmt");
+
+        }
+        //write-stmt ->write expr ;
+        else {
+            ExecutorFactory factory = ExecutorFactory.getExecutorFactory();
+            Executor exeExpr = factory.getExecutor(root.getChild(1), env);
+            Object[] exeValue1 = (Object[]) exeExpr.Execute(root.getChild(1));
+            String writeString = (String) exeValue1[1];
+
+        }
+        return null;
+    }
 }
+
