d006e6655cd012efaaa259500194d4eefd237c27
WHU-Yh-jx <1092949763@qq.com>
2019-10-31 16:56:49 +0800

    10-31-hjx-Assign.While.Write

--- src/main/java/interpreter/executor/subExecutor/WriteStmt.java
+++ src/main/java/interpreter/executor/subExecutor/WriteStmt.java
@@ -1,11 +1,16 @@
 package interpreter.executor.subExecutor;
 
 import interpreter.env.Env;
+import interpreter.exception.ExecutionError;
+import interpreter.exception.SemanticError;
 import interpreter.executor.BaseExecutor;
 import interpreter.executor.Executor;
 import interpreter.executor.ExecutorFactory;
 import interpreter.grammar.lalr.LALRNonterminalSymbol;
 import interpreter.intermediate.node.INode;
+import interpreter.intermediate.type.BasicType;
+import interpreter.intermediate.type.DataType;
+import message.Message;
 
 public class WriteStmt extends BaseExecutor {
     public WriteStmt(Env env) {
@@ -16,15 +21,25 @@ public class WriteStmt extends BaseExecutor {
     public Object Execute(INode root) throws Exception {
         if (!root.getSymbol().equals(LALRNonterminalSymbol.WRITE_STMT)) {
             System.out.println("parse error in write stmt");
-
         }
         //write-stmt ->write expr ;
         else {
             ExecutorFactory factory = ExecutorFactory.getExecutorFactory();
             Executor exeExpr = factory.getExecutor(root.getChild(1), env);
             Object[] exeValue1 = (Object[]) exeExpr.Execute(root.getChild(1));
-            String writeString = (String) exeValue1[1];
-
+            DataType resultType=(DataType) exeValue1[0];
+            //如果表达式返回值可以被write
+            if(env.writeCompatible(resultType))
+            {
+                Message message = new Message(Message.MessageType.WRITE, exeValue1[1]);
+                postMessage(message);
+            }
+            //不可则报类型不匹配错误
+            else
+            {
+                throw ExecutionError.newWriteUnmatchTypeError(resultType,
+                        (Integer) root.getChild(1).getAttribute(INode.INodeKey.LINE));
+            }
         }
         return null;
     }
