21022d657bc06e7d1de2dee0facf1f2ff3872138
zjj <632291793@qq.com>
2019-11-01 11:18:15 +0800

    1031-backend: modify executor test

--- src/test/java/ExecutorTest.java
+++ src/test/java/ExecutorTest.java
@@ -1,8 +1,16 @@
 import org.junit.Test;
 import interpreter.executor.subExecutor.Expr;
 public class ExecutorTest {
+
+    @Test
+    public void typeTest() {
+        Integer i = 9;
+        System.out.println(i.doubleValue());
+    }
+
     @Test
     public void envTest() {
        // Expr e=new Expr();
+
     }
 }
