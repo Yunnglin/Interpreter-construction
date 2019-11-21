f2254647d3982fde7a5f1181ee469edc74b4f58f
Yunlin <mao.looper@qq.com>
2019-11-21 15:55:50 +0800

    1120-myl-add find variable

--- src/main/java/FrontEnd/DebuggerForm.java
+++ src/main/java/FrontEnd/DebuggerForm.java
@@ -7,6 +7,7 @@ import interpreter.intermediate.sym.SymTbl;
 import interpreter.intermediate.sym.SymTblEntry;
 import interpreter.intermediate.type.BasicType;
 import interpreter.intermediate.type.DataType;
+import interpreter.intermediate.type.TypeForm;
 import message.Message;
 import message.MessageListener;
 
@@ -24,13 +25,14 @@ public class DebuggerForm {
     private MainWindow mainWindow;
     private ArrayList<SymTblEntry> symTblEntries;
     private Interpreter interpreter;
+    private String PLACE_HOLDER = "请输入变量名,按回车键查看";
 
-    public DebuggerForm(MainWindow mainWindow){
+    public DebuggerForm(MainWindow mainWindow) {
         this.mainWindow = mainWindow;
     }
 
     public void init() {
-        inputTextField.addFocusListener(new JTextFieldHintListener("请输入变量名,按回车键查看", inputTextField));
+        inputTextField.addFocusListener(new JTextFieldHintListener(PLACE_HOLDER, inputTextField));
         inputTextField.addKeyListener(new DebuggerMessageListener());
         outTextArea.setEditable(false);
         this.interpreter = mainWindow.getmButton().getInterpreter();
@@ -44,39 +46,71 @@ public class DebuggerForm {
             frame.setVisible(true);
         });
     }
-    public void close(){
+
+    public void close() {
         this.frame.dispose();
     }
 
-    private void findEntries(){
+    private void findEntries() {
         //symTblEntries = new ArrayList<>();
+        outTextArea.setText("");
+        if (inputTextField.getText().equals(PLACE_HOLDER) || inputTextField.getText().equals("")) {
+            return;
+        }
         String[] strings = inputTextField.getText().trim().split(";");
-        for(String s : strings){
-            SymTblEntry entry =this.interpreter.findSymTblEntry(s.trim());
-            if(entry!=null){
+        for (String s : strings) {
+            SymTblEntry entry = this.interpreter.findSymTblEntry(s.trim());
+            if (entry != null) {
                 appendLine(entryToString(entry));
-            }else{
-                appendLine("未找到变量: "+s);
+            } else {
+                appendLine("未找到变量: " + s);
             }
         }
     }
 
-    private String entryToString(SymTblEntry symTblEntry){
+    private String entryToString(SymTblEntry symTblEntry) {
         DataType type = (DataType) symTblEntry.getValue(SymTbl.SymTblKey.TYPE);
-        Object valie = symTblEntry.getValue(SymTbl.SymTblKey.VALUE);
+        Object value = symTblEntry.getValue(SymTbl.SymTblKey.VALUE);
         String name = symTblEntry.getName();
         StringBuilder builder = new StringBuilder();
-//        switch (type.getBasicType()){
-//            case BasicType.INT:{
-//                break;
-//            }
-//        }
-        return "";
+        builder.append("变量名：").append(name + '\t');
+        if (type.getBasicType().equals(BasicType.INT)) {
+            if (type.getForm().equals(TypeForm.SCALAR)) {
+                int curValue = (int) value;
+                builder.append("类型: 整数\t");
+                builder.append("值: " + curValue);
+            } else {
+                int size = (int)symTblEntry.getValue(SymTbl.SymTblKey.ARRAY_SIZE);
+                Object[] curValue = (Object[]) value;
+                builder.append("类型: 整数数组\t");
+                builder.append("长度: "+size+"\t");
+                builder.append("值: ");
+                for (Object cur : curValue) {
+                    builder.append(String.valueOf(cur==null?0:(int)cur) + '\t');
+                }
+            }
+        } else if (type.getBasicType().equals(BasicType.REAL)) {
+            if (type.getForm().equals(TypeForm.SCALAR)) {
+                double curValue = (double) value;
+                builder.append("类型: 实数\t");
+                builder.append("值: " + curValue);
+            } else {
+                int size = (int)symTblEntry.getValue(SymTbl.SymTblKey.ARRAY_SIZE);
+                Object[] curValue = (Object[]) value;
+                builder.append("类型: 实数数组\t");
+                builder.append("长度: "+size+"\t");
+                builder.append("值: ");
+                for (Object cur : curValue) {
+                    builder.append(String.valueOf(cur==null?0:(double)cur) + '\t');
+                }
+            }
+        }
+        return builder.toString();
     }
 
 
-    private void appendLine(String s){
-        outTextArea.setText(outTextArea.getText()+s+'\n');
+    private void appendLine(String s) {
+        outTextArea.setText(outTextArea.getText() + s + '\n');
     }
 
     private class DebuggerMessageListener implements MessageListener, KeyListener {
@@ -104,7 +138,6 @@ public class DebuggerForm {
         public void onMessageReceived(Message message) {
             Message.MessageType type = message.getType();
             if (type == Message.MessageType.SUSPEND_ON_TRAP) {
-                outTextArea.setText("");
                 findEntries();
             }
         }
